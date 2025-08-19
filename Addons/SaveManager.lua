bd94f4b3e6ab653c7fa56bb3bb3c46770495b0f4
cloudman4416 <134954873+cloudman4416@users.noreply.github.com>
2025-08-20 00:50:09 +0200

    Update SaveManager.lua

--- Addons/SaveManager.lua
+++ Addons/SaveManager.lua
@@ -294,7 +294,7 @@ local SaveManager = {} do
 
 		local AutoloadButton
 		AutoloadButton = section:AddButton({Title = "Set as autoload", Description = "Current autoload config: none", Callback = function()
-			local name = SaveManager.Options.SaveManager_ConfigList.Value
+			local name = SaveManager.Options.SaveManager_ConfigList.Value or "none"
 			writefile(self.Folder .. "/settings/autoload.txt", name)
 			AutoloadButton:SetDesc("Current autoload config: " .. name)
 			self.Library:Notify({
