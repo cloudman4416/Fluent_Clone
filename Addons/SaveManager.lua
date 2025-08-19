a8697d7216ec929bef272d21d1513f0077dd2449
cloudman4416 <134954873+cloudman4416@users.noreply.github.com>
2025-08-20 00:55:51 +0200

    Update SaveManager.lua

--- Addons/SaveManager.lua
+++ Addons/SaveManager.lua
@@ -295,14 +295,27 @@ local SaveManager = {} do
 		local AutoloadButton
 		AutoloadButton = section:AddButton({Title = "Set as autoload", Description = "Current autoload config: none", Callback = function()
 			local name = SaveManager.Options.SaveManager_ConfigList.Value
-			writefile(self.Folder .. "/settings/autoload.txt", name)
-			AutoloadButton:SetDesc("Current autoload config: " .. name)
-			self.Library:Notify({
-				Title = "Interface",
-				Content = "Config loader",
-				SubContent = string.format("Set %q to auto load", name),
-				Duration = 7
-			})
+			if name then
+				writefile(self.Folder .. "/settings/autoload.txt", name)
+				AutoloadButton:SetDesc("Current autoload config: " .. name)
+				self.Library:Notify({
+					Title = "Interface",
+					Content = "Config loader",
+					SubContent = string.format("Set %q to auto load", name),
+					Duration = 7
+				})
+			else
+				if isfile(self.Folder .. "/settings/autoload.txt") then
+					delfile(self.Folder .. "/settings/autoload.txt")
+				end
+				AutoloadButton:SetDesc("Current autoload config: none")
+				self.Library:Notify({
+					Title = "Interface",
+					Content = "Config loader",
+					SubContent = "Set none to auto load",
+					Duration = 7
+				})
+			end
 		end})
 
 		if isfile(self.Folder .. "/settings/autoload.txt") then
